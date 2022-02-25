54c1aa48cbb886f3f63d31785b6a4f109870225f
maxcore25 <mkorobov2002@mail.ru>
2022-02-25 07:07:05 +0300

    Toolbar copied

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
-import Head from 'next/head';
-import Image from 'next/image';
 import styles from '../styles/Home.module.css';
+import { Toolbar } from '../components/toolbar';
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className='page-container'>
+      <Toolbar />
+
+      <div className={styles.main}>
+        <h1>Next.js News App</h1>
+
+        <h3>Your one stop shop for the latest news articles</h3>
+      </div>
+    </div>
+  );
 }
