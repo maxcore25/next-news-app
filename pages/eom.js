7e6268ea381eeefb8780cc94bf816ed77cf11a79
maxcore25 <mkorobov2002@mail.ru>
2022-02-25 06:54:41 +0300

    API added and JSX copied

--- pages/eom.js
+++ pages/eom.js
@@ -1,11 +1,62 @@
-const EOM = () => {
+import Head from 'next/head';
+// import styles from '../styles/EOM.module.css';
+// import { Toolbar } from '../components/toolbar';
+
+const EOM = ({ employee }) => {
+  console.log(employee);
   return (
-    <div className='page-container'>
-      <div>
-        <h1>Employee Of The Month</h1>
+    <>
+      <Head>
+        <title>Employee Of The Month</title>
+        <meta
+          name='description'
+          content={`This month's employee of the month is ${employee.name}`}
+        />
+
+        <meta property='og:image' content={employee.image} />
+        <meta property='og:title' content='Employee Of The Month' />
+        <meta
+          property='og:description'
+          content={`This month's employee of the month is ${employee.name}`}
+        />
+
+        <meta property='twitter:image' content={employee.image} />
+        <meta property='twitter:title' content='Employee Of The Month' />
+        <meta
+          property='twitter:description'
+          content={`This month's employee of the month is ${employee.name}`}
+        />
+      </Head>
+
+      <div className='page-container'>
+        {/* <Toolbar /> */}
+
+        <div className={styles.main}>
+          <h1>Employee Of The Month</h1>
+
+          <div className={styles.employeeOfTheMonth}>
+            <h3>{employee.name}</h3>
+            <h6>{employee.position}</h6>
+            <img src={employee.image} alt='employee' />
+            <p>{employee.description}</p>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
+};
+
+export const getServerSideProps = async pageContext => {
+  const apiResponse = await fetch(
+    'https://my-json-server.typicode.com/portexe/next-news/employeeOfTheMonth'
+  );
+  const employee = await apiResponse.json();
+
+  return {
+    props: {
+      employee,
+    },
+  };
 };
 
 export default EOM;
