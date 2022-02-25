3e82e4ed7e63a8ad6b3bb27852b2df7079d4f8d0
maxcore25 <mkorobov2002@mail.ru>
2022-02-25 06:36:53 +0300

    README deleted and EOM page created

---
+++ pages/eom.js
@@ -0,0 +1,11 @@
+const EOM = () => {
+  return (
+    <div className='page-container'>
+      <div>
+        <h1>Employee Of The Month</h1>
+      </div>
+    </div>
+  );
+};
+
+export default EOM;
